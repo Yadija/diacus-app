72baa69c907917bd55e199b6e41382a36fbaa451
Yadija <ayuliadi01@gmail.com>
2023-09-24 08:41:10 +0800

    ⚒️build: use api threads in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from 'next/image';
 
-export default function Home() {
+async function getData() {
+  const response = await fetch('http://localhost:5000/threads', {
+    cache: 'no-store',
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const responseJson = await response.json();
+  return responseJson.data;
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main className='relative m-auto min-h-screen max-w-xl bg-[#F0F0F0]'>
       <nav className='bg-[#213555] p-4 text-[#F0F0F0]'>
@@ -22,25 +37,27 @@ export default function Home() {
       </form>
 
       <ul className='m-4 flex flex-col gap-2'>
-        <li>
-          <section className='flex w-full gap-2 bg-[#E5D283] px-2 py-4'>
-            <div className='relative h-12 w-12 rounded-full bg-[#213555]'>
-              <Image
-                src='https://ui-avatars.com/api/?name=johndoe&background=random'
-                alt='johndoe'
-                fill={true}
-                className='rounded-full object-cover'
-              />
-            </div>
-            <article className='flex-1'>
-              <section className='flex justify-between text-sm'>
-                <h2>Username</h2>
-                <p className='text-[#4F709C]'>Date</p>
+        {data.threads.map(
+          (thread: { id: string; content: string; owner: string }) => (
+            <li key={thread.id}>
+              <section className='flex w-full gap-2 bg-[#E5D283] px-2 py-4'>
+                <div className='relative h-12 w-12 rounded-full bg-[#213555]'>
+                  <Image
+                    src={`https://ui-avatars.com/api/?name=${thread.owner}&background=random`}
+                    alt={thread.owner}
+                    fill={true}
+                    sizes='100%'
+                    className='rounded-full object-cover'
+                  />
+                </div>
+                <article className='flex-1'>
+                  <h2 className='text-sm'>@{thread.owner}</h2>
+                  <p className='text-2xl'>{thread.content}</p>
+                </article>
               </section>
-              <p className='text-2xl'>content</p>
-            </article>
-          </section>
-        </li>
+            </li>
+          )
+        )}
       </ul>
 
       <section className='fixed bottom-0 w-[36rem] bg-slate-400 py-3'>

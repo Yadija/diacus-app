53261f853df3b6e8d585f5ad9b4ade72ef974e44
Yadija <ayuliadi01@gmail.com>
2023-10-04 14:29:23 +0800

    📦refactor: handling in login and register

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
-import useInput from '@/hooks/useInput';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
-import { signIn, useSession, signOut } from 'next-auth/react';
+import useInput from '@/hooks/useInput';
 
 export default function Login() {
   const session = useSession();
@@ -14,9 +14,15 @@ export default function Login() {
   const [password, onPasswordChange] = useInput('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  // if (session.status === 'authenticated') {
-  //   router.push('/');
-  // }
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      router?.push('/');
+    }
+  }, [session]);
+  
+  if (session.status === 'loading') {
+    return <p>Loading ...</p>;
+  }
 
   const onLogin = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -31,46 +37,48 @@ export default function Login() {
       setErrorMessage('Invalid username or password');
       return;
     }
-
-    router.push('/');
   };
 
-  return (
-    <main className='m-auto flex min-h-screen max-w-xl items-center justify-center bg-[#F0F0F0]'>
-      <form
-        className='flex w-[400px] flex-col gap-4 bg-[#213555] p-4'
-        onSubmit={onLogin}
-      >
-        <h1 className='text-center text-2xl font-bold text-[#F0F0F0]'>Login</h1>
-        {errorMessage && (
-          <p className='translate-y-3 text-sm text-red-500 shadow-md'>
-            *{errorMessage}
+  if (session.status === 'unauthenticated') {
+    return (
+      <main className='m-auto flex min-h-screen max-w-xl items-center justify-center bg-[#F0F0F0]'>
+        <form
+          className='flex w-[400px] flex-col gap-4 bg-[#213555] p-4'
+          onSubmit={onLogin}
+        >
+          <h1 className='text-center text-2xl font-bold text-[#F0F0F0]'>Login</h1>
+          {errorMessage && (
+            <p className='translate-y-3 text-sm text-red-500 shadow-md'>
+              *{errorMessage}
+            </p>
+          )}
+          <input
+            className='px-2 py-1'
+            type='text'
+            placeholder='username'
+            onChange={onUsernameChange}
+            required
+          />
+          <input
+            className='px-2 py-1'
+            type='password'
+            placeholder='password'
+            onChange={onPasswordChange}
+            required
+          />
+          <button type='submit' className='mt-8 bg-[#E5D283] p-2 font-bold'>
+            Login
+          </button>
+          <p className='text-center text-[#F0F0F0]'>
+            {`Don't have an account? `}
+            <Link href='/register' className='text-[#E5D283]'>
+              Register
+            </Link>
           </p>
-        )}
-        <input
-          className='px-2 py-1'
-          type='text'
-          placeholder='username'
-          onChange={onUsernameChange}
-          required
-        />
-        <input
-          className='px-2 py-1'
-          type='password'
-          placeholder='password'
-          onChange={onPasswordChange}
-          required
-        />
-        <button type='submit' className='mt-8 bg-[#E5D283] p-2 font-bold'>
-          Login
-        </button>
-        <p className='text-center text-[#F0F0F0]'>
-          {`Don't have an account? `}
-          <Link href='/register' className='text-[#E5D283]'>
-            Register
-          </Link>
-        </p>
-      </form>
-    </main>
-  );
+        </form>
+      </main>
+    );
+  } else {
+    return null;
+  }
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import useInput from '@/hooks/useInput';
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 
 const register = async ({
   username,
@@ -36,12 +37,23 @@ const register = async ({
 };
 
 export default function Register() {
+  const session = useSession();
   const router = useRouter();
 
   const [username, onUsernameChange] = useInput('');
   const [fullname, onFullnameChange] = useInput('');
   const [password, onPasswordChange] = useInput('');
   const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    if (session.status === 'authenticated') {
+      router?.push('/');
+    }
+  }, [session]);
+  
+  if (session.status === 'loading') {
+    return <p>Loading ...</p>;
+  }
 
   const onRegister = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -60,51 +72,55 @@ export default function Register() {
     router.push('/login');
   };
 
-  return (
-    <main className='m-auto flex min-h-screen max-w-xl items-center justify-center bg-[#F0F0F0]'>
-      <form
-        className='flex w-[400px] flex-col gap-4 bg-[#213555] p-4'
-        onSubmit={onRegister}
-      >
-        <h1 className='text-center text-2xl font-bold text-[#F0F0F0]'>
-          Register
-        </h1>
-        {errorMessage && (
-          <p className='translate-y-3 text-sm text-red-500 shadow-md'>
-            *{errorMessage}
+  if (session.status === 'unauthenticated') {
+    return (
+      <main className='m-auto flex min-h-screen max-w-xl items-center justify-center bg-[#F0F0F0]'>
+        <form
+          className='flex w-[400px] flex-col gap-4 bg-[#213555] p-4'
+          onSubmit={onRegister}
+        >
+          <h1 className='text-center text-2xl font-bold text-[#F0F0F0]'>
+            Register
+          </h1>
+          {errorMessage && (
+            <p className='translate-y-3 text-sm text-red-500 shadow-md'>
+              *{errorMessage}
+            </p>
+          )}
+          <input
+            className='px-2 py-1'
+            type='text'
+            placeholder='username'
+            onChange={onUsernameChange}
+            required
+          />
+          <input
+            className='px-2 py-1'
+            type='text'
+            placeholder='fullname'
+            onChange={onFullnameChange}
+            required
+          />
+          <input
+            className='px-2 py-1'
+            type='password'
+            placeholder='password'
+            onChange={onPasswordChange}
+            required
+          />
+          <button type='submit' className='mt-8 bg-[#E5D283] p-2 font-bold'>
+            Register
+          </button>
+          <p className='text-center text-[#F0F0F0]'>
+            {`Already have an account? `}
+            <Link href='/login' className='text-[#E5D283]'>
+              Login
+            </Link>
           </p>
-        )}
-        <input
-          className='px-2 py-1'
-          type='text'
-          placeholder='username'
-          onChange={onUsernameChange}
-          required
-        />
-        <input
-          className='px-2 py-1'
-          type='text'
-          placeholder='fullname'
-          onChange={onFullnameChange}
-          required
-        />
-        <input
-          className='px-2 py-1'
-          type='password'
-          placeholder='password'
-          onChange={onPasswordChange}
-          required
-        />
-        <button type='submit' className='mt-8 bg-[#E5D283] p-2 font-bold'>
-          Register
-        </button>
-        <p className='text-center text-[#F0F0F0]'>
-          {`Already have an account? `}
-          <Link href='/login' className='text-[#E5D283]'>
-            Login
-          </Link>
-        </p>
-      </form>
-    </main>
-  );
+        </form>
+      </main>
+    );
+  } else {
+    return null;
+  };
 }

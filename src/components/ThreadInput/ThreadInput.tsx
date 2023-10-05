976d291ec8b58d970153b373b4348485daf99e52
Yadija <ayuliadi01@gmail.com>
2023-10-05 09:46:02 +0800

    ⚒️build: implement thread input with axios private

--- src/components/ThreadInput/ThreadInput.tsx
+++ src/components/ThreadInput/ThreadInput.tsx
@@ -1,65 +1,28 @@
 'use client';
 
+import React, { FormEvent } from 'react';
+import { useSession } from 'next-auth/react';
+import useAxiosPrivate from '@/hooks/useAxiosPrivate';
 import useInput from '@/hooks/useInput';
-import React, { FormEvent, useEffect, useState } from 'react';
-
-async function postThread(content: string) {
-  const accessToken = localStorage.getItem('accessToken');
-  console.log(accessToken);
-
-  const response = await fetch('http://localhost:5000/threads', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'content-type': 'application/json',
-    },
-    cache: 'no-store',
-    body: JSON.stringify({
-      content,
-    }),
-  });
-
-  if (!response.ok) {
-    alert('Failed to fetch data');
-  }
-
-  const responseJson = await response.json();
-  return responseJson.data;
-}
 
 const ThreadInput = () => {
+  const session = useSession();
+  const axiosPrivate = useAxiosPrivate();
   const [content, onContentChange] = useInput('');
 
-  const onCreateThread = async (content: string) => {
-    const data = await postThread(content);
-    console.log(data);
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await axiosPrivate.post('/threads', JSON.stringify({ content }));
   };
 
-  const handlereateThread = async (event: FormEvent<HTMLFormElement>) => {
-    // event.preventDefault();
-
-    await onCreateThread(content);
-    // router.refresh();
-  };
-
-  const [auth, setAuth] = useState(false);
-
-  useEffect(() => {
-    const accessToken = localStorage.getItem('accessToken');
-
-    if (accessToken) {
-      setAuth(true);
-    }
-  }, []);
-
-  if (!auth) {
+  if (session.status === 'loading' || session.status === 'unauthenticated') {
     return null;
   }
 
   return (
     <form
       className='mx-2 my-4 flex w-full justify-center'
-      onSubmit={handlereateThread}
+      onSubmit={handleSubmit}
     >
       <input
         type='text'
